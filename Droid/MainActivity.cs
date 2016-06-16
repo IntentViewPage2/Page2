64673a45b919c64657bb7c2c6fdd6c7396ef24d8
蔡孟錡 <mengchi.tsai@jetfusion.com.tw>
2016-06-16 18:15:47 +0800

    * Page2.Droid.csproj: * Main.axml: * Resource.designer.cs: * sample_7.png: * sample_6.png: * sample_5.png: * sample_0.jpg: * sample_3.png: * sample_2.png: * sample_1.png: * sample_0.png: * sample_4.png:
    * MainActivity.cs: Gridview

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Views;
+using Android.Graphics;
 
 namespace Page2.Droid
 {
@@ -16,14 +19,71 @@ namespace Page2.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			var gridview = FindViewById<GridView> (Resource.Id.gridview);
+			gridview.Adapter = new ImageAdapter (this);
+
+
+			gridview.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
+				Toast.MakeText (this, args.Position.ToString (), ToastLength.Short).Show ();
 			};
 		}
+	}
+
+	public class ImageAdapter : BaseAdapter
+	{
+		Context context;
+
+		public ImageAdapter (Context c)
+		{
+			context = c;
+		}
+
+		public override int Count {
+			get { return thumbIds.Length; }
+		}
+
+		public override Java.Lang.Object GetItem (int position)
+		{
+			return null;
+		}
+
+		public override long GetItemId (int position)
+		{
+			return 0;
+		}
+
+		// create a new ImageView for each item referenced by the Adapter
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			ImageView imageView;
+
+			if (convertView == null) {  // if it's not recycled, initialize some attributes
+				imageView = new ImageView (context);
+				imageView.LayoutParameters = new GridView.LayoutParams (125, 125);
+				imageView.SetScaleType (ImageView.ScaleType.CenterCrop);
+				imageView.SetPadding (8, 8, 8, 8);
+			} else {
+				imageView = (ImageView)convertView;
+			}
+
+			imageView.SetImageResource (thumbIds[position]);
+			return imageView;
+		}
+
+		// references to our images
+		int[] thumbIds = {
+			Resource.Drawable.sample_0 ,Resource.Drawable.sample_0,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+			Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+			Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+			Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+			Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7
+		};
 	}
 }
 
